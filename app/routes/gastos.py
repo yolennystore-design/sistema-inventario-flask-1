da743a68ce35a8ade693ff3df440070cfa8e44e3
unknown <yolennystore@gmail.com>
2026-01-20 12:45:05 -0400

    Add gastos module only for admin

---
+++ app/routes/gastos.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template, session, redirect, url_for
+
+gastos_bp = Blueprint("gastos", __name__, url_prefix="/gastos")
+
+
+@gastos_bp.route("/")
+def index():
+    # 🔒 Solo administrador
+    if session.get("rol") != "admin":
+        return redirect(url_for("dashboard"))
+
+    return render_template("gastos/index.html")
